d2e37170b1ca65c922a622f76f6d4a322a00e335
Iman Tabrizian <Tabrizian@outlook.com>
2016-08-30 17:32:34 +0430

    Now we store hashs

--- app/Http/Controllers/SendController.php
+++ app/Http/Controllers/SendController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Device;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,5 +16,12 @@ class SendController extends Controller
         ];
 
         return view('partials.device.create', compact('devices'));
+    }
+
+    function store(Request $request) {
+        $hash = $request->json('hash');
+        $device = new Device();
+        $device->hash = $hash;
+        $device->save();
     }
 }
--- resources/views/partials/device/create.blade.php
+++ resources/views/partials/device/create.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts/layout')
 
 @section('content')
-    <ul class="list-group">
-        @foreach($devices as $device)
-            <li class="list-group-item">{{ $device['hash'] }}</li>
-        @endforeach
-    </ul>
+    <div class="row">
+        <div class="col-xs-3">
+            <h1>List of devices</h1>
+            <ul class="list-group">
+                @foreach($devices as $device)
+                    <li class="list-group-item">{{ $device['hash'] }}</li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 @stop
