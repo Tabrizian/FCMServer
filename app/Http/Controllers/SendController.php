44673a5ffb614ee2cdab8d9ef45674039efb929c
Iman Tabrizian <Tabrizian@outlook.com>
2016-08-31 15:42:15 +0430

    Completes forms

--- app/Http/Controllers/SendController.php
+++ app/Http/Controllers/SendController.php
@@ -10,10 +10,7 @@ use App\Http\Requests;
 class SendController extends Controller
 {
     function create() {
-        $devices = [
-            ['hash' => 'Salam'],
-            ['hash' => 'What']
-        ];
+        $devices = Device::all();
 
         return view('partials.device.create', compact('devices'));
     }
--- resources/views/partials/device/create.blade.php
+++ resources/views/partials/device/create.blade.php
@@ -6,9 +6,18 @@
             <h1>List of devices</h1>
             <ul class="list-group">
                 @foreach($devices as $device)
-                    <li class="list-group-item">{{ $device['hash'] }}</li>
+                    <li class="list-group-item" id="{{$device['id']}}">{{ substr($device['hash'], 0, 10) }}
+                        <div class="checkbox pull-right">
+                            <input type="checkbox" value="" style="margin-bottom: 20px">
+                        </div>
+                    </li>
                 @endforeach
             </ul>
+            <div class="form-group">
+                <label class="form-control">Message to be sent</label>
+                <input class="form-control" type="text">
+            </div>
+            <button type="submit" class="btn btn-primary">Send message</button>
         </div>
     </div>
 @stop
